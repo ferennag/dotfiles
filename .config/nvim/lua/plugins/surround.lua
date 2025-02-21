fb9f4f9e63fca85cafd926e25e913c38e31205c9
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2025-02-20 19:33:31 -0500

    NVim config

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,12 @@
+return {
+    'echasnovski/mini.surround',
+    opts = {
+        mappings = {
+            add = '<leader>sa',
+            delete = '<leader>sd',
+            find = '<leader>sf',
+            find_left = '<leader>sF',
+            replace = '<leader>sr',
+        }
+    }
+}
