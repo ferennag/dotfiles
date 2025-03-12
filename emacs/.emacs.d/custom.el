b3c9539972e6e2d0cca6404e4ed361c64bfa7a72
Ferenc Nagy <ferenc.nagy.jr@protonmail.com>
2025-03-11 22:37:44 -0400

    Add evil mode

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,10 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("eca44f32ae038d7a50ce9c00693b8986f4ab625d5f2b4485e20f22c47f2634ae"
+     default))
  '(package-selected-packages
-   '(cape catppuccin-theme catpuccin-theme consult corfu embark
-	  embark-consult magit marginalia nerd-icons
-	  nerd-icons-completion nerd-icons-corfu orderless
+   '(cape catppuccin-theme catpuccin-theme consult corfu doom-themes
+	  embark embark-consult evil god-mode magit marginalia meow
+	  nerd-icons nerd-icons-completion nerd-icons-corfu orderless
 	  rainbow-delimiters use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -31,12 +31,11 @@
   (electric-pair-mode t)
   (set-frame-font "JetBrainsMono Nerd Font Mono 14" nil t))
 
-(setq auto-save-file-name-transforms
-  `((".*" "~/.emacs-saves/" t)))
-
-(use-package catppuccin-theme
+(use-package doom-themes
   :config
-  (load-theme 'catppuccin :no-confirm))
+  (load-theme 'doom-dracula))
+
+(use-package catppuccin-theme)
 
 (use-package vertico
   :init
@@ -168,3 +167,7 @@
 (setq gc-cons-threshold (* 2 1000 1000))
 ;; Increase the amount of data which Emacs reads from the process
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+(use-package evil
+  :config
+  (evil-mode 1))
